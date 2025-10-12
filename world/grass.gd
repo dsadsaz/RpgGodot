1148f63b772317baca838e960b454594c5af8bd4
GiuseppeTringale <g.tringale@contrader.it>
2025-10-12 17:08:36 +0200

    end of video 3

---
+++ world/grass.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+const GRASS_EFFECT = preload("res://effects/grass_effect.tscn")
+
+@onready var area_2d: Area2D = $Area2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	area_2d.area_entered.connect(_on_area_2d_area_entered)
+	pass # Replace with function body.
+
+
+func _on_area_2d_area_entered(other_area_2d: Area2D) -> void:
+	var grass_effect_instance = GRASS_EFFECT.instantiate()
+	get_tree().current_scene.add_child(grass_effect_instance)
+	grass_effect_instance.global_position = global_position
+	queue_free()
